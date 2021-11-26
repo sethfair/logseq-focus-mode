84454035a7f41fa373c2dfd54476077ed767bf6d
Seth Fair <seth@sethfair.com>
2021-11-25 23:26:58 -0600

    add hotkey setting through ui

--- src/index.js
+++ src/index.js
@@ -1,11 +1,97 @@
 /** @jsx h */
 import '@logseq/libs'
 import './index.css';
-import {h, render} from 'preact'
+import {h, render, createRef} from 'preact'
 import {useEffect, useState} from 'preact/hooks'
 
+let keyboardTimeout;
+let appendKey = false;
+
+const HotkeyEditor = ({setSettings}) => {
+    const [enableHotkeySet, setEnableHotkey] = useState(false)
+    const [hotkey, setHotkey] = useState(logseq.settings.keyboard)
+    const [previousHotKey, setPreviousHotkey] = useState(false)
+    const [shouldAppendKey, setAppendKey] = useState(false)
+    const hotkeyInputField = createRef();
+
+    const handleSetHotKey = e => {
+        e.preventDefault();
+        const previousAppend = shouldAppendKey;
+        if (keyboardTimeout) clearTimeout(keyboardTimeout);
+
+        keyboardTimeout = setTimeout(() => {
+            console.log('timeout')
+            setAppendKey(false);
+        }, 250);
+        setAppendKey(true);
+
+
+        if (previousAppend) {
+            setHotkey(`${hotkey} ${e.key}`);
+        } else {
+            if (e.ctrlKey) {
+                // setHotkey(`ctrl+${e.key.toString()}`)
+            } else {
+                setHotkey(e.key);
+            }
+        }
+    };
+
+    const handleCancelBtn = () => {
+        console.log('handleCancelBtn');
+        setHotkey(previousHotKey);
+        setEnableHotkey(false);
+    }
+
+    const clearHotKey = () => {
+        setPreviousHotkey(hotkey);
+        setHotkey('');
+    }
+
+    const handleKeyPress = (e) => {
+        console.log('handleKeyPress', e.key, e, enableHotkeySet);
+        if(enableHotkeySet) handleSetHotKey(e);
+    }
+
+    const handleSave = (e) => {
+        const newState = Object.assign({}, logseq.settings);
+        newState['keyboard'] = hotkey;
+        console.log(newState);
+        setSettings(newState);
+        setEnableHotkey(false)
+
+        if(confirm('Restart of Logseq is required for the hotkey change to take effect.')) {
+            logseq.App.relaunch();
+        }
+    }
+
+    const handleInputClick = () => {
+        console.log('handleInputClick')
+        if (!enableHotkeySet) {
+            setEnableHotkey(true);
+            clearHotKey();
+        }
+    }
+
+    const handleEditClick = () => {
+        console.log('handle edit click');
+        if (hotkeyInputField && hotkeyInputField.current) {
+            hotkeyInputField.current.focus();
+            setEnableHotkey(true);
+        }
+    }
+
+    return <div>
+        <input ref={hotkeyInputField} value={hotkey} className={`hotkeybox ${enableHotkeySet && "enabled"}`}
+               onClick={handleInputClick} onFocus={handleInputClick} onKeyPress={handleKeyPress} />
+        {!enableHotkeySet && <button className="hotkey-btn edit-hotkey" onClick={handleEditClick}>edit</button>}
+        {enableHotkeySet && <button className="hotkey-btn edit-hotkey" onClick={handleSave}>save</button>}
+        {enableHotkeySet && <button className="hotkey-btn cancel-hotkey" onClick={handleCancelBtn}>cancel</button>}
+    </div>;
+}
+
 function App() {
-    const [settings, setSettings] = useState()
+    const [settings, setSettings] = useState({})
 
     useEffect(() => {
         setSettings(logseq.settings);
@@ -18,8 +104,8 @@ function App() {
     const OptionCheck = ({label, name}) => {
         return (<div className={"option-check"}>
                 <label>{label}</label>
-                <input checked={settings && settings[name]} name={name} type="checkbox" onClick={(e) => {
-                    const newState = Object.assign({}, settings);
+                <input checked={logseq.settings && logseq.settings[name]} name={name} type="checkbox" onClick={(e) => {
+                    const newState = Object.assign({}, logseq.settings);
                     newState[name] = e.target.checked === true;
                     setSettings(newState);
                 }}/>
@@ -47,6 +133,15 @@ function App() {
                 <legend>On Unfocus</legend>
                 <OptionCheck name="open_left_sidebar_on_unfocus" label="Show Left Sidebar"/>
                 <OptionCheck name="open_right_sidebar_on_unfocus" label="Show Right Sidebar"/>
+            </fieldset>
+
+            <fieldset>
+                <legend>Keyboard Shortcut</legend>
+                <div>
+                    <p class={'instructions'}>Click in the box below and type the key combination you wish to use to
+                        toggle focus mode.</p>
+                    <HotkeyEditor setSettings={setSettings}/>
+                </div>
             </fieldset>
 
             <p className="ctl">
@@ -129,17 +224,19 @@ function main() {
         width: '290px',
         zIndex: 999,
         transform: 'translateX(-50%)',
-    })
-
-    logseq.App.registerCommandPalette({
-        key: 'toggle_focus_mode_shortcut',
-        label: 'Toggle Focus Mode Keyboard Shortcut',
-        keybinding: {
-            binding: 'f f'
-        }
-    }, () => {
-        model.toggleFocus();
     });
+
+    if (logseq.settings.keyboard) {
+        logseq.App.registerCommandPalette({
+            key: 'toggle_focus_mode_shortcut',
+            label: 'Toggle Focus Mode Keyboard Shortcut',
+            keybinding: {
+                binding: logseq.settings.keyboard
+            }
+        }, () => {
+            model.toggleFocus();
+        });
+    }
 
     logseq.App.registerUIItem('toolbar',
         {
